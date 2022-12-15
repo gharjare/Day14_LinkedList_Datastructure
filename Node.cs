b0b4de91709203ed3e2f228d45b98f03e3253b0a
gharjare <gharjarechetan@gmail.com>
2022-12-15 10:56:29 +0530

    add node file

---
+++ Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureLinkedList
+{
+    public class Node
+    {
+        public int id;
+        public Node next;
+        public string data;
+
+        public Node(int id)
+
+        {
+            this.id = id;
+        }
+    }
+}
