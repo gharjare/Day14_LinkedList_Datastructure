02531f79441ba4c1ccf32192f7c5bd1f91fe8b60
gharjare <gharjarechetan@gmail.com>
2022-12-15 11:09:06 +0530

    insert the elemnt  element

---
+++ LinkedList3.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureLinkedList
+{
+    public class LinkedList3
+    {
+        internal Node head;
+        public void Add(int id)
+        {
+            Node node = new Node(id);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+
+                    temp.next = node;
+
+                }
+
+            }
+            Console.WriteLine("{0} is inserted into linked list", node.id);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+
+            }
+        }
+
+        public void insert()
+        {
+            if(this.head == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("{0} is top on the stack", this.head.id);
+        }
+    }
+}
